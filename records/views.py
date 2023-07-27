ad31355c045ce3ddf5d12ea3054163d31e5cfd63
YiHungWONG <40423264+yihong1120@users.noreply.github.com>
2023-07-28 04:45:18 +0800

    Add files via upload

--- records/views.py
+++ records/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
-def records(request):
+def history(request):
     if request.user.is_authenticated:
-        return render(request, 'records/records.html')
+        return render(request, 'records/history.html')
     else:
         form = AuthenticationForm()
         return render(request, 'accounts/login.html', {'form': form})
