bba130897e7d9eb081c8240c8cbab07c8c07e1ad
YiHungWONG <40423264+yihong1120@users.noreply.github.com>
2023-07-29 22:31:58 +0800

    Add files via upload

--- VisionSnap/views.py
+++ VisionSnap/views.py
@@ -1,16 +1,116 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.serializers.json import DjangoJSONEncoder
+from django.views.decorators.csrf import csrf_exempt
+from pytube import YouTube
+from urllib.parse import urlparse
+import base64
+import json
+import cv2
+import os
+import requests
+from PIL import Image
+from io import BytesIO
+
+# from .detection_predictor import class_names
+from .yolov8 import YOLOInference
 
 def index(request):
+    allPredData = ["item1", "item2", "item3", "item4"]
+    selectedPredData = ["item5", "item6", "item7", "item8"]
+    shownPredData = {
+    0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 5: 'bus',
+    6: 'train', 7: 'truck'
+    }
+
+    allPredData_json = json.dumps(allPredData, cls=DjangoJSONEncoder)
+    selectedPredData_json = json.dumps(selectedPredData, cls=DjangoJSONEncoder)
+    
     if request.user.is_authenticated:
-        return render(request, 'VisionSnap/logged_in.html', {'username': request.user.username})
+        return render(request, 'VisionSnap/logged_in.html', {
+            'username': request.user.username, 
+            'pred_class_names': YOLOInference.get_class_names(), 
+            'allPredData':allPredData_json, 
+            'selectedPredData':selectedPredData_json,
+            'shownPredData': shownPredData
+        }) 
     else:
-        return render(request, 'VisionSnap/logged_out.html')
+        form = AuthenticationForm()
+        return render(request, 'accounts/login.html', {'form': form})
+    
+def get_url_content_type(url):
+    try:
+        # 檢查網址是否有效
+        response = requests.head(url)
+        if not response.ok:
+            return "Invalid URL"
+        
+        # 判斷是否為YouTube的URL
+        if 'youtube' in url or 'youtu.be' in url:
+            yt = YouTube(url)
+            if yt.streams.filter(only_video=True).first() is not None:
+                return "video"
+            else:
+                return "unknown"
+            
+        else:
+            # 檢查網址的副檔名
+            parsed = urlparse(url)
+            ext = os.path.splitext(parsed.path)[1]
+            image_exts = ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.webp', '.tiff']
+            video_exts = ['.mp4', '.avi', '.mov', '.flv', '.wmv', '.mkv']
+            if ext in image_exts:
+                return "image"
+            elif ext in video_exts:
+                return "video"
+            else:
+                return "unknown"
+
+    except (requests.exceptions.RequestException, Exception) as e:
+        return str(e)
+
+@csrf_exempt
+def process_url(request):
+    if request.method == 'POST':
+        url = request.POST.get('url')
+
+        if get_url_content_type(url) == 'image':
+            print("oui, c'est une image.")
+
+            # Image process
+            yolo_inference = YOLOInference('models/yolov8x.pt')
+            detections, orig_img, img_path = yolo_inference.process_image(url)
+            image = yolo_inference.draw_boxes(orig_img, detections, YOLOInference.get_class_names())
+
+            # 將 numpy array 轉換為 PIL image
+            image_pil = Image.fromarray(image)
+
+            buffered = BytesIO()
+            image_pil.save(buffered, format="JPEG")
+            img_str = base64.b64encode(buffered.getvalue()).decode()
+            os.remove(img_path)
+
+            # 將 Base64 字串包裝在 JSON 物件中並返回到前端
+            return JsonResponse({'status': 'success', 'image': img_str})
+
+        elif get_url_content_type(url) == 'video':
+            print("c'est un vidéo.")
+
+            return JsonResponse({'status': 'success'})
+    else:
+        return JsonResponse({'status': 'failed'})
 
 def controls(request):
-    return render(request, 'VisionSnap/controls.html')
-
-def records(request):
-    return render(request, 'VisionSnap/records.html')
+    if request.user.is_authenticated:
+        return render(request, 'VisionSnap/controls.html')
+    else:
+        form = AuthenticationForm()
+        return render(request, 'accounts/login.html', {'form': form})
 
 def logout(request):
-    return render(request, 'VisionSnap/logout.html')
+    if request.user.is_authenticated:
+        return render(request, 'VisionSnap/logout.html')
+    else:
+        form = AuthenticationForm()
+        return render(request, 'accounts/login.html', {'form': form})
--- VisionSnap/yolov8.py
+++ VisionSnap/yolov8.py
@@ -1,10 +1,69 @@
-from ultralytics import YOLO
+import cv2
+import ultralytics
 
-# Load a model
-model = YOLO("yolov8x.yaml")  # build a new model from scratch
-model = YOLO("yolov8x.pt")  # load a pretrained model (recommended for training)
+class YOLOInference:
+    def __init__(self, model_path):
+        self.model = ultralytics.YOLO(model_path)
 
-# Use the model
-results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
+    def process_video(self, source):
+        results = self.model(source, stream=True)
 
-print(results)
+        for result in results:
+            print(result.boxes.data)
+
+    def process_image(self, source):
+        results = self.model(source)  # not streaming for a single image
+        detections = results[0].boxes.data
+        orig_img = results[0].orig_img
+        img_path = results[0].path
+
+        return detections, orig_img, img_path
+
+    @staticmethod
+    def draw_boxes(image, detections, class_names):
+        for detection in detections:
+            x1, y1, x2, y2, confidence, class_id = detection
+            class_name = class_names[int(class_id)]
+            color = (0, 255, 0)  # Green color for the bounding box
+            thickness = 2
+            cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), color, thickness)
+            label = f"{class_name}: {confidence:.2f}"
+            cv2.putText(image, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+        return image
+
+    @staticmethod
+    def get_class_names():
+        return {
+            0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus',
+            6: 'train', 7: 'truck', 8: 'boat', 9: 'traffic light', 10: 'fire hydrant',
+            11: 'stop sign', 12: 'parking meter', 13: 'bench', 14: 'bird', 15: 'cat',
+            16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear',
+            22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag',
+            27: 'tie', 28: 'suitcase', 29: 'frisbee', 30: 'skis', 31: 'snowboard',
+            32: 'sports ball', 33: 'kite', 34: 'baseball bat', 35: 'baseball glove',
+            36: 'skateboard', 37: 'surfboard', 38: 'tennis racket', 39: 'bottle',
+            40: 'wine glass', 41: 'cup', 42: 'fork', 43: 'knife', 44: 'spoon', 45: 'bowl',
+            46: 'banana', 47: 'apple', 48: 'sandwich', 49: 'orange', 50: 'broccoli',
+            51: 'carrot', 52: 'hot dog', 53: 'pizza', 54: 'donut', 55: 'cake', 56: 'chair',
+            57: 'couch', 58: 'potted plant', 59: 'bed', 60: 'dining table', 61: 'toilet',
+            62: 'tv', 63: 'laptop', 64: 'mouse', 65: 'remote', 66: 'keyboard', 67: 'cell phone',
+            68: 'microwave', 69: 'oven', 70: 'toaster', 71: 'sink', 72: 'refrigerator',
+            73: 'book', 74: 'clock', 75: 'vase', 76: 'scissors', 77: 'teddy bear',
+            78: 'hair drier', 79: 'toothbrush'
+        }
+
+
+if __name__ == '__main__':
+    yolo_inference = YOLOInference('models/yolov8x.pt')
+    # detections, orig_img = yolo_inference.process_image('Tallinn.png')
+    
+    # url = 'https://www.youtube.com/watch?v=z_mlibCfgFI&ab_channel=%E6%A1%83%E5%9C%92%E6%99%BA%E6%85%A7%E6%97%85%E9%81%8A%E9%9B%B2TaoyuanTravel'
+    url = 'https://finnoytravel.com/images/Tallinn_Christmas_market_family_visitors.jpg'
+    detections, orig_img = yolo_inference.process_image(url)
+
+    image = yolo_inference.draw_boxes(orig_img, detections, YOLOInference.get_class_names())
+
+    # Display the image
+    cv2.imshow('Object Detection', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
